07a1ed072b15fda0f0dbd7b3d2da8b49ecfca03f
Walee@ANDIT <webguru071@gmail.com>
2020-03-18 16:56:35 +0600

    2020/Mar/18

--- lib/MainScreen/Dashboard/ExpansionTIleTests.dart
+++ lib/MainScreen/Dashboard/ExpansionTIleTests.dart
@@ -2,104 +2,96 @@ import 'package:flutter/material.dart';
 import 'package:flutterhms/SplashScreen/Typo.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-
 class ExpansionTileTest extends StatefulWidget {
   @override
-  _MyHomePageScreenState createState() => new _MyHomePageScreenState();
+  _ExpansionTileTestState createState() => _ExpansionTileTestState();
 }
 
-class _MyHomePageScreenState extends State<ExpansionTileTest> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("testing"),
-      ),
-      drawer: Drawer(
-        child: ListView.builder(
-          itemCount: policies.length,
-          itemBuilder: (context, i) {
-            return new ExpansionTile(
-              title: new Text(
-                policies[i].title,
-                style: new TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.blue),
-              ),
-              children: <Widget>[
-                new Column(
-                  children: _buildExpandableContent(policies[i]),
-                ),
-              ],
-            );
-          },
-        ),
-      ),
-      body: Text('ERROR'),
-    );
-  }
-
-  _buildExpandableContent(Websites policies) {
-    List<Widget> columnContent = [];
-
-    for (String content in policies.contents)
-      columnContent.add(
-        new ListTile(
-            title: new Text(
-              content,
-              style: new TextStyle(fontSize: 18.0, color: Colors.lightBlue),
-            ),
-            onTap: () {
-              _openWebUrl(Websites.endpoints[content], content);
-            }),
-      );
-
-    return columnContent;
-  }
-
-  _openWebUrl(String url, String title) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => GeneralWebScreen(url: url, item: title),
-      ),
-    );
-  }
-}
-
-class Websites {
-  final String title;
-  List<dynamic> contents = [];
-
-  static final Map<String, dynamic> endpoints = {"git": Box()};
-
-  Websites(this.title, this.contents);
-}
-
-List<Websites> policies = [
-  new Websites('websites sample 1', ['git', 'flutter']),
-  new Websites('Websites sample 2', ['google', 'swift']),
-];
-
-
-class GeneralWebScreen extends StatelessWidget {
-  final String url;
-  final String item;
-
-  GeneralWebScreen({Key key, @required this.url, @required this.item})
-      : super(key: key);
+class _ExpansionTileTestState extends State<ExpansionTileTest> {
+  bool expanded = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(item),
+      appBar: AppBar(
+        title: Text('Pushing data'),
+      ),
+      drawer: Drawer(
+        child: Card(
+          child: Column(
+            children: <Widget>[
+              ExpansionPanelList(
+                expansionCallback: (int index, bool status) {
+                  if (status == true) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: new Text('Alert Dialog'),
+                          content: new Text('What do you want to do?'),
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text('Cancel'),
+                              onPressed: () => Navigator.of(context).pop(),
+                            ),
+                            FlatButton(
+                              child: Text(
+                                'Collapse',
+                                style: TextStyle(color: Colors.red),
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  expanded = false;
+                                });
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  } else {
+                    setState(() {
+                      expanded = true;
+                    });
+                  }
+                },
+                children: [
+                  ExpansionPanel(
+                    canTapOnHeader: true,
+                    isExpanded: expanded,
+                    headerBuilder: (BuildContext context, bool expanded) {
+                      return Text('Expansion Panel Header');
+                    },
+                    body: Center(
+
+
+                        child: Text('Expanded panel body'),
+
+                    ),
+                  ),
+                  ExpansionPanel(
+                    canTapOnHeader: true,
+                    isExpanded: expanded,
+                    headerBuilder: (BuildContext context, bool expanded) {
+                      return Text('Expansion Panel Header');
+                    },
+                    body: Center(
+
+
+                      child: Text('Expanded panel body'),
+
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        body: WebView(
-          initialUrl: url,
-          javascriptMode: JavascriptMode.unrestricted,
-        ));
+      ),
+      body: Center(
+          child: Text("test"),
+      ),
+      );
   }
 }
