263efb499863d10aa6ed9e5d7dace36de3b180b4
Walee <nafeewalee@gmail.com>
2020-04-09 16:44:18 +0600

    sorting, flitering and alert message

--- lib/MainScreen/Dashboard/TransactionTable.dart
+++ lib/MainScreen/Dashboard/TransactionTable.dart
@@ -11,12 +11,12 @@ class TransactionTable extends StatefulWidget {
 }
 
 class _TransactionTableState extends State<TransactionTable> {
-  int idFlag = 0,dateFlag = 0,amountFlag=0,transFlag=0;
+  int idFlag = 0, dateFlag = 0, amountFlag = 0, transFlag = 0;
   TextEditingController controller = new TextEditingController();
+  String filter;
 
   // Get json result and convert it to model. Then add
   Future<Null> getFutureTransDetails() async {
-    print('hello');
     final response = await http.get(url);
     final responseJson = json.decode(response.body);
 
@@ -31,8 +31,20 @@ class _TransactionTableState extends State<TransactionTable> {
   @override
   void initState() {
     super.initState();
+    _transDetails.clear();
+      getFutureTransDetails();
 
-    getFutureTransDetails();
+    controller.addListener(() {
+      setState(() {
+        filter = controller.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -47,7 +59,6 @@ class _TransactionTableState extends State<TransactionTable> {
                   controller: controller,
                   decoration: new InputDecoration(
                       hintText: 'Search', border: InputBorder.none),
-                  onChanged: onSearchTextChanged,
                 ),
                 trailing: new IconButton(
                   icon: new Icon(Icons.close),
@@ -62,63 +73,60 @@ class _TransactionTableState extends State<TransactionTable> {
           Card(
             color: Theme.of(context).cursorColor,
             child: Container(
-              padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+              padding: EdgeInsets.only(left: 10, top: 15, bottom: 15),
               child: Row(
                 children: <Widget>[
-
                   Expanded(
                     flex: 1,
-                    child:  new GestureDetector(
+                    child: new GestureDetector(
                       onTap: () {
-                        setState(() { if(idFlag==0){
-                          _transDetails.sort((a, b) => a.id.compareTo(b.id));
-                          idFlag=1;
-                        }
-                        else{
-                          _transDetails.sort((b, a) => a.id.compareTo(b.id));
-                          idFlag=0;
-                        }
+                        setState(() {
+                          if (idFlag == 0) {
+                            _transDetails.sort((a, b) => a.id.compareTo(b.id));
+                            idFlag = 1;
+                          } else {
+                            _transDetails.sort((b, a) => a.id.compareTo(b.id));
+                            idFlag = 0;
+                          }
                         });
-
                       },
-                        child: new Text("ID"),
-
+                      child: new Text("ID"),
                     ),
                   ),
-              Expanded(
-                flex: 2,
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() { if(dateFlag==0){
-                      _transDetails.sort((a, b) => a.transType.compareTo(b.transType));
-                      dateFlag=1;
-                    }
-                    else{
-                      _transDetails.sort((b, a) => a.transType.compareTo(b.transType));
-                      dateFlag=0;
-                    }
-                    });
-
-                  },
-                  child: Text("TransID"),
-                ),
-              ),
-
-
                   Expanded(
                     flex: 2,
                     child: GestureDetector(
                       onTap: () {
-                        setState(() { if(transFlag==0){
-                          _transDetails.sort((a, b) => a.transId.compareTo(b.transId));
-                          transFlag=1;
-                        }
-                        else{
-                          _transDetails.sort((b, a) => a.transId.compareTo(b.transId));
-                          transFlag=0;
-                        }
+                        setState(() {
+                          if (dateFlag == 0) {
+                            _transDetails.sort(
+                                (a, b) => a.transType.compareTo(b.transType));
+                            dateFlag = 1;
+                          } else {
+                            _transDetails.sort(
+                                (b, a) => a.transType.compareTo(b.transType));
+                            dateFlag = 0;
+                          }
                         });
-
+                      },
+                      child: Text("TransID"),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (transFlag == 0) {
+                            _transDetails
+                                .sort((a, b) => a.transId.compareTo(b.transId));
+                            transFlag = 1;
+                          } else {
+                            _transDetails
+                                .sort((b, a) => a.transId.compareTo(b.transId));
+                            transFlag = 0;
+                          }
+                        });
                       },
                       child: Text("TransType"),
                     ),
@@ -127,16 +135,17 @@ class _TransactionTableState extends State<TransactionTable> {
                     flex: 2,
                     child: GestureDetector(
                       onTap: () {
-                        setState(() { if(amountFlag==0){
-                          _transDetails.sort((a, b) => a.amount.compareTo(b.amount));
-                          amountFlag=1;
-                        }
-                        else{
-                          _transDetails.sort((b, a) => a.amount.compareTo(b.amount));
-                          amountFlag=0;
-                        }
+                        setState(() {
+                          if (amountFlag == 0) {
+                            _transDetails
+                                .sort((a, b) => a.amount.compareTo(b.amount));
+                            amountFlag = 1;
+                          } else {
+                            _transDetails
+                                .sort((b, a) => a.amount.compareTo(b.amount));
+                            amountFlag = 0;
+                          }
                         });
-
                       },
                       child: Text("Amount"),
                     ),
@@ -146,48 +155,49 @@ class _TransactionTableState extends State<TransactionTable> {
             ),
           ),
           new Expanded(
-            child: _searchResult.length != 0 || controller.text.isNotEmpty
-                ? new ListView.builder(
-                    itemCount: _searchResult.length,
-                    itemBuilder: (context, i) {
-                      return new Card(
-                        child: Container(
-                          padding:
-                              EdgeInsets.only(left: 10, top: 10, bottom: 10),
-                          child: Row(
-                            children: <Widget>[
-                              Expanded(
-                                flex: 1,
-                                child: Text(_transDetails[i].id.toString()),
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: Text(_transDetails[i].transId),
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: Text(_transDetails[i].transType),
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: Text(_transDetails[i].amount),
-                              ),
-
-                            ],
-                          ),
+            child: new ListView.builder(
+                itemCount: _transDetails.length,
+                itemBuilder: (BuildContext context, int index) {
+                  var f = _transDetails[index];
+                  //return new Card(child: new Text(_transDetails[index].id.toString()));
+                  if (filter == null || filter == "") {
+                    return new Card(
+                        child: new InkWell(
+                          onTap: () {
+                            _showDialog(context,index);
+                          },
+                      child: Container(
+                        padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: Text(f.id.toString()),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.transId),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.transType),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.amount.toString()),
+                            ),
+                          ],
                         ),
-                      );
-                    },
-                  )
-                : new ListView.builder(
-              shrinkWrap: true,
-                    itemCount: _transDetails.length,
-                    itemBuilder: (context, index) {
-                      var f = _transDetails[index];
+                      ),
+                    ),);
+                  } else if(_transDetails[index].id.toString().toLowerCase().contains(filter.toLowerCase())) {
                       return new Card(
+                          child: new InkWell(
+                            onTap: () {
+                              _showDialog(context,index);
+                            },
                         child: Container(
-                          padding:
-                              EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                          padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
                           child: Row(
                             children: <Widget>[
                               Expanded(
@@ -204,15 +214,103 @@ class _TransactionTableState extends State<TransactionTable> {
                               ),
                               Expanded(
                                 flex: 2,
-                                child: Text(f.amount),
+                                child: Text(f.amount.toString()),
                               ),
-
                             ],
                           ),
                         ),
-                      );
+                      ),);
+                    } else if(_transDetails[index].transId.toLowerCase().contains(filter.toLowerCase())) {
+                    return new Card(
+                        child: new InkWell(
+                          onTap: () {
+                            _showDialog(context,index);
+                          },
+                      child: Container(
+                        padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: Text(f.id.toString()),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.transId),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.transType),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.amount.toString()),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),);
+                  }else if(_transDetails[index].transType.toLowerCase().contains(filter.toLowerCase())) {
+                    return new Card(
+                      child: Container(
+                        padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: Text(f.id.toString()),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.transId),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.transType),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.amount.toString()),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }else if(_transDetails[index].amount.toString().toLowerCase().contains(filter.toLowerCase())) {
+                    return new Card(
+                        child: new InkWell(
+                        onTap: () {
+                      _showDialog(context,index);
                     },
-                  ),
+                      child: Container(
+                        padding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: Text(f.id.toString()),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.transId),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.transType),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Text(f.amount.toString()),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),);
+                  }else {
+                      return new Container();
+                    }
+
+                }),
           ),
         ],
       ),
@@ -225,13 +323,6 @@ class _TransactionTableState extends State<TransactionTable> {
       setState(() {});
       return;
     }
-
-    _transDetails.forEach((transDetail) {
-      if (transDetail.transType.contains(text.toLowerCase()) ||
-          transDetail.transType.toLowerCase().contains(text.toUpperCase()))
-        _searchResult.add(transDetail);
-    });
-    setState(() {});
   }
 }
 
@@ -244,8 +335,8 @@ final String url =
 
 class TransactionDetails {
   final int id;
-  final String transType, transId,description;
-  final String amount;
+  final String transType, transId, description;
+  final double amount;
 
   TransactionDetails({
     this.id,
@@ -257,11 +348,36 @@ class TransactionDetails {
 
   factory TransactionDetails.fromJson(Map<String, dynamic> json) {
     return new TransactionDetails(
-      id: json['id'],
-      transId: json['trans_id'],
-      transType: json['transaction_type'],
-      amount: json['amount'],
-      description: json['description']
-    );
+        id: json['id'],
+        transId: json['trans_id'],
+        transType: json['transaction_type'],
+        amount:  double.parse(json['amount']),
+        description: json['description']);
   }
+}
+void _showDialog(BuildContext context, index) {
+// flutter defined function
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      // return object of type Dialog
+      return AlertDialog(
+        title: new Text(_transDetails[index].id.toString()),
+        content: new Text("Do you wish to log out?"),
+        actions: <Widget>[
+          new FlatButton(
+            child: new Text("Logout"),
+            onPressed: () {
+            },
+          ),
+          new FlatButton(
+            child: new Text("Stay"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
